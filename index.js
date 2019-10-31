5fd44015829f431b3a396f36d01f691ea4950043
JiayiZhaoUofT <jiayizhao27@gmail.com>
2019-10-30 21:46:58 -0400

    refactor the server code structure

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
 const app = express();
-
-// create new instance of google strategy
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
-      console.log("done", done);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+//make sure the passport.js is executed, so we need to require it
+require("./services/passport");
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
