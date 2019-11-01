d889dc4c008117c217bc95b7a67adeb99aca902d
JiayiZhaoUofT <jiayizhao27@gmail.com>
2019-10-31 22:24:28 -0400

    connect to the remote mongo server

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const app = express();
-//make sure the passport.js is executed, so we need to require it
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+require("./models/User");
+
+const app = express();
+mongoose.connect(keys.mongoURI);
+
+//make sure the passport.js is executed, so we need to require it
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
