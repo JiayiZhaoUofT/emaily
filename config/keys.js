f77edaf55458c67cd620c50e55db8a6b89462c60
JiayiZhaoUofT <jiayizhao27@gmail.com>
2019-11-05 23:35:30 -0500

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "262525632621-23tfqbojmm9sd4qoen7otrjdfgokdjtq.apps.googleusercontent.com",
+  googleClientSecret: "R-leldc4OpOS1ofs_DyQK5ZE",
+  mongoURI:
+    "mongodb+srv://dbUser1:Pk1g0xfs5KbTRX7Y@emaily-dev-q08rw.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "islaksueospw"
+};
